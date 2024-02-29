f4afb63b2d928ad1becbf003f2b563cf2dbf620b
Alejandro Baez <abaez2623@gmail.com>
2024-02-29 14:35:30 -0500

    CI react test

--- server/db/database.js
+++ server/db/database.js
@@ -1,7 +1,10 @@
 //establishing connection
 const { Sequelize } = require("sequelize");
-const db = new Sequelize("postgres://localhost:5432/campus_student_db", {
-  logging: false,
-});
+const db = new Sequelize(
+  "postgres://postgres:ajj2623y@localhost:5432/campus_student_db",
+  {
+    logging: false,
+  }
+);
 
 module.exports = db;
