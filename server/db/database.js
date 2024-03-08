969c236fb71ae3f544625633eb5478713ece0a5b
Alejandro Baez <abaez2623@gmail.com>
2024-03-08 01:01:23 -0500

    Added database url

--- server/db/database.js
+++ server/db/database.js
@@ -1,7 +1,7 @@
 //establishing connection
 const { Sequelize } = require("sequelize");
 const db = new Sequelize(
-  "postgres://postgres:ajj2623y@127.0.0.1:5432/campus_student_db",
+  process.env.DATABASE_URL || "postgres://postgres:ajj2623y@127.0.0.1:5432/campus_student_db",
   {
     logging: false,
   }
